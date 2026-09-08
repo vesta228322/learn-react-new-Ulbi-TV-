fd78d607935472307573ea3d790474ccb12d8854
vesta228322 <hotabych39@gmail.com>
2026-09-08 19:13:17 +0300

    feat: added modal, modified post-list and post-item

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,16 +1,37 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import PostItem from './PostItem'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
 function PostList({ posts, title, remove }) {
+    const nodeRef = useRef(null);
+
+    if (!posts.length) {
+        return (
+            <h1
+                style={{ textAlign: 'center' }} >
+                Посты не найдены
+            </h1>
+        )
+    }
+
+
     return (
         <div>
             <h1 style={{ textAlign: 'center' }} >
                 {title}
             </h1>
-            {posts.map((post, index) =>
-                <PostItem remove={remove} number={index + 1} post={post} key={post.id} />
-            )}
-
+            <TransitionGroup>
+                {posts.map((post, index) =>
+                    <CSSTransition
+                        key={post.id}
+                        nodeRef={nodeRef}
+                        timeout={500}
+                        classNames="post"
+                    >
+                        <PostItem remove={remove} number={index + 1} post={post} />
+                    </CSSTransition>
+                )}
+            </TransitionGroup>
         </div>
     )
 }
